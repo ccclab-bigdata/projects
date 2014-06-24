7e016a5a464865eaa207e4003ca2bf1089266d1e
gostopa1 <gostopa1@aalto.fi>
2014-06-24 12:37:12 +0300

    Removed some files

--- ctps/functions/logsigder.m
+++
@@ -1,7 +0,0 @@
-function [ Y ] = logsigder( X )
-%LOGSIGDER Summary of this function goes here
-%   Detailed explanation goes here
-Y=logsig(X).*(1-logsig(X));
-
-end
-
--- ctps/functions/tanhder.m
+++
@@ -1,7 +0,0 @@
-function [ Y ] = tanhder( X )
-%TANHDER Summary of this function goes here
-%   Detailed explanation goes here
-Y=1./(cosh(X).^2);
-
-end
-
